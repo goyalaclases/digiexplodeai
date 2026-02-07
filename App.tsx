3ec761549cb86de587a80080e31cc63477ec0ce8
goyalaclases <goyalaclasses@gmail.com>
2026-02-07 18:36:44 +0530

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 
 import React, { useEffect } from 'react';
 import { HashRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
-import Header from './components/Layout/Header';
+import Header from "./src/components/Layout/Header";
 import Footer from './components/Layout/Footer';
 import Home from './pages/Home';
 import Projects from './pages/Projects';
